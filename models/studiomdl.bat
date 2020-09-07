8132ad568a2f1df2b11b148f8d31fb955bffc176
Mariana <marianacsanjurjo@hotmail.com>
2020-09-07 13:05:40 -0700

    Add heart

--- models/studiomdl.bat
+++ models/studiomdl.bat
@@ -1,1 +1,3 @@
-"C:\Program Files (x86)\Steam\steamapps\common\Half-Life SDK\Model Tools\studiomdl.exe" "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\full_life\models\cqscript.qc"
+"C:\Program Files (x86)\Steam\steamapps\common\Half-Life SDK\Model Tools\studiomdl.exe" "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\full_life\models\goose.qc"
+
+"C:\Program Files (x86)\Steam\steamapps\common\Half-Life SDK\Model Tools\studiomdl.exe" "C:\Program Files (x86)\Steam\steamapps\common\Half-Life\full_life\models\heart.qc"
